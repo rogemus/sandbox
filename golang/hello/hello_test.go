16c5c7f52ecda8bdbed78f96f0a3f39a93df9f0f
Kacper Rogowski <rogemus@gmail.com>
2024-02-23 22:28:02 +0100

    master small test refactor

--- golang/hello/hello_test.go
+++ golang/hello/hello_test.go
@@ -8,17 +8,20 @@ func TestHello(t *testing.T) {
 		got := Hello("Kacper")
 		want := "Hello Kacper"
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello World' when empty name", func(t *testing.T) {
 		got := Hello("")
-		want := "Hello World"
+		want := "Hello Wo2rld"
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
 }
