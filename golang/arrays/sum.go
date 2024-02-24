baf0a6696bfba9822706bc914b1f20f0fe6f4dc6
Kacper Rogowski <rogemus@gmail.com>
2024-02-24 21:58:12 +0100

    master Add sumAll

--- golang/arrays/sum.go
+++ golang/arrays/sum.go
@@ -1,11 +1,24 @@
 package arrays
 
 func Sum(numbers []int) int {
-  sum := 0
-  
-  for _, number := range numbers {
-    sum += number
-  }
+	sum := 0
 
-  return sum
+	for _, number := range numbers {
+		sum += number
+	}
+
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+
+func SumAllTails(numbersToSum ...[]int) []int {
+  return nil
 }
--- golang/arrays/sum_test.go
+++ golang/arrays/sum_test.go
@@ -2,21 +2,10 @@ package arrays
 
 import (
 	"testing"
+  "reflect"
 )
 
 func TestSum(t *testing.T) {
-	t.Run("Collection of 5 numbers", func(t *testing.T) {
-
-		numbers := []int{1, 2, 3, 4, 5}
-
-		got := Sum(numbers)
-		want := 15
-
-		if got != want {
-			t.Errorf("want %d, got %d | %v", want, got, numbers)
-		}
-	})
-
 	t.Run("Collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 
@@ -28,4 +17,22 @@ func TestSum(t *testing.T) {
 		}
 	})
 
+}
+
+func TestSumAll(t *testing.T) {
+  got := SumAll([]int{1, 2}, []int{0, 9})
+  want := []int{3, 9}
+
+  if !reflect.DeepEqual(got, want) {
+    t.Errorf("want %v, got %v", want, got)
+  }
+}
+
+func TestSumAllTails(t *testing.T) {
+  got := SumAllTails([]int{1, 2}, []int{0, 9})
+  want := []int{2, 9}
+
+  if !reflect.DeepEqual(got, want) {
+    t.Errorf("want %v, got %v", want, got)
+  }
 }
