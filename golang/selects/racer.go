b135f8d2d16397b20d7153b94e759039c3e027a2
Kacper Rogowski <rogemus@gmail.com>
2024-02-26 23:59:31 +0100

    master Selects

---
+++ golang/selects/racer.go
@@ -0,0 +1,39 @@
+package selects
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
+}
+
+var tenSecondTime = 10 * time.Second
+
+func Racer(a, b string) (winner string, err error) {
+	return ConfifurableRacer(a, b, tenSecondTime)
+}
+
+func ConfifurableRacer(a, b string, timeout time.Duration) (winner string, err error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
+}
