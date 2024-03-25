296eebd7e78926d8f40e7c01ae2459eafd51ff7b
Kacper Rogowski <rogemus@gmail.com>
2024-03-25 14:47:56 +0100

    master Add basic template

--- golang/tuts/frontendmaster/museum/main.go
+++ golang/tuts/frontendmaster/museum/main.go
@@ -2,23 +2,34 @@ package main
 
 import (
 	"fmt"
+	"museum/data"
 	"net/http"
+	"text/template"
 )
 
 func handleHello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hellow from go program"))
 }
 
+func handleTemplate(w http.ResponseWriter, r *http.Request) {
+	html, err := template.ParseFiles("templates/index.tmpl")
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error"))
+		return
+	}
+	html.Execute(w, data.GetAll())
+}
+
 func main() {
 	server := http.NewServeMux()
 	server.HandleFunc("/hello", handleHello)
+	server.HandleFunc("/template", handleTemplate)
 
-
-  /* */
-  fs := http.FileServer(http.Dir("./public"))
-  server.Handle("/", fs)
-
-
+	/* */
+	fs := http.FileServer(http.Dir("./public"))
+	server.Handle("/", fs)
 
 	err := http.ListenAndServe(":3333", server)
 
